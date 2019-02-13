a06206f331b9c0938468c4812882962764a0c3e3
chinedumifeanyi <kingsolomonifeanyi@gmail.com>
2019-02-13 20:39:41 +0100

    fix

--- server/models/dbSetup.js
+++ server/models/dbSetup.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 let dbConnect = mysql.createConnection({
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_NAME || 'expense'
+	host: process.env.DBHOST || 'localhost',
+	user: process.env.DBUSER || 'root',
+	password: process.env.DBPASSWORD || '',
+	database: process.env.DBNAME || 'expense'
 });
 
 
