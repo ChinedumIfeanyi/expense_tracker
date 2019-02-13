9f32cf1d4bfdb4244f1eb8ef5dd69d77ba8b6497
chinedumifeanyi <kingsolomonifeanyi@gmail.com>
2019-02-13 20:48:27 +0100

    fix database error

--- build/models/dbSetup.js
+++ build/models/dbSetup.js
@@ -11,10 +11,10 @@ var _mysql2 = _interopRequireDefault(_mysql);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var dbConnect = _mysql2.default.createConnection({
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_NAME || 'expense'
+	host: process.env.DBHOST || 'localhost',
+	user: process.env.DBUSER || 'root',
+	password: process.env.DBPASSWORD || '',
+	database: process.env.DBNAME || 'expense'
 });
 
 exports.default = dbConnect;
