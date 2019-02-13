704243dc4f6a9ef562ac75d2ca372b57b01aa737
chinedumifeanyi <kingsolomonifeanyi@gmail.com>
2019-02-13 14:23:01 +0100

    prepare for production

--- server/models/dbSetup.js
+++ server/models/dbSetup.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 let dbConnect = mysql.createConnection({
-	host: process.env.DATABASE_HOST || 'localhost',
-	user: process.env.DATABASE_USER || 'root',
-	password: process.env.DATABASE_PASSWORD || '',
-	database: process.env.DATABASE_URL || 'expense'
+	host: process.env.DB_HOST || 'localhost',
+	user: process.env.DB_USER || 'root',
+	password: process.env.DB_PASSWORD || '',
+	database: process.env.DB_NAME || 'expense'
 });
 
 dbConnect.connect((err)=>{
